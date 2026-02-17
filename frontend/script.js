701329e1cd8aac570e3aef03f6a58f5cca4b239f
Gowtham Sai Gona <gonagowtham94@gmail.com>
2026-02-17 20:26:59 +0530

    Updated frontend to use live backend URL

--- frontend/script.js
+++ frontend/script.js
@@ -46,7 +46,7 @@ async function analyzeResume() {
 
     try {
 
-        const response = await fetch("http://127.0.0.1:5000/analyze", {
+        const response = await fetch("http://resumematch-ai-uocz.onrender.com/analyze", {
             method: "POST",
             body: formData
         });
